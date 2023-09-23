4fd61906b71370816acdf9f8e84368b4cb4bd732
Hunter Antal <treehunter519@gmail.com>
2023-09-23 14:12:43 -0400

    Initial Commit

---
+++ Assignment-1-2412/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Assignment-1-2412
+//
+//  Created by Hunter Antal on 2023-09-23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
