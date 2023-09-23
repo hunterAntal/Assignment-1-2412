d0fbc7e3c2319630fc2c1e9aa40c07b99414acee
Hunter Antal <treehunter519@gmail.com>
2023-09-23 14:23:13 -0400

    Added main to file.

--- Assignment-1-2412/main.c
+++ Assignment-1-2412/main.c
@@ -7,8 +7,30 @@
 
 #include <stdio.h>
 
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    struct Queue *qptr = NULL;
+    
+    build(&qptr, 5);
+    // ====== Beginn Test Case 1
+    enqueue(qptr, 1);
+    enqueue(qptr, 2);
+    enqueue(qptr, 3);
+    enqueue(qptr, 4);
+    enqueue(qptr, 5);
+    enqueue(qptr, 6);
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    // ====== End Test Case 1
+    
+    // free anything that was created with malloc!
+    if (qptr->data) free(qptr->data);
+    if(qptr) free(qptr);
+    printf("\n");
     return 0;
 }
